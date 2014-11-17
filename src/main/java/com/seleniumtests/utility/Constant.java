66c5e80dcf5b1b3f502ee9d35874056569a3183d
RaviJindal <rjindal@xebia.com>
2014-11-17 17:31:09 +0530

    Update Constant.java

--- src/main/java/com/seleniumtests/utility/Constant.java
+++ src/main/java/com/seleniumtests/utility/Constant.java
@@ -1,7 +1,7 @@
 package com.seleniumtests.utility;
 
 public class Constant {
-	 
+//
     public static final String URL = "http://tbbqabeta.productpartners.com";
     public static final String Path_TestData = "C:\\D\\MySelWorkspaceBackups\\seleniumtestsbb\\src\\main\\resources";
     public static final String File_TestData = "TestData.xlsx";
